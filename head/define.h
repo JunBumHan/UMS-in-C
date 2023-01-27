e06ffa351ef0ef85d0520c9cdbe0551e50f2acc3
JunBumHan <2hanjunbum6@gmail.com>
2023-01-28 01:42:25 +0900

    ADD::FIX::헤더파일을 추가하고 디렉토리의 구조를 수정했습니다.

---
+++ head/define.h
@@ -0,0 +1,15 @@
+#ifndef DEFINE_H
+# define DEFINE_H
+
+
+// SCN[Sniffer code number defines]
+// [ERROR] (0 ~ 255)
+#define SCN_ERROR_SOCKET    0x0000000F
+#define SCN_ERROR_BIND      0x000000F0
+#define SCN_ERROR_ACCEPT    0x00000F00            
+
+// [BUG] (256 ~ 512)
+
+// [EXCEPTION] (513 ~ 768)
+
+#endif
---
+++ head/untils.h
@@ -0,0 +1,12 @@
+#ifndef UNTILS_H
+# define UNTILS_H
+
+
+// [sniffer_code]
+
+// <sniffer_code.c>
+void sniffied(int SCN);
+
+// <>
+
+#endif
