213efd21acd4fa743df871bb01e0d8b935f96898
JunBumHan <2hanjunbum6@gmail.com>
2023-01-28 14:12:09 +0900

    UPD::코드의 현재 상황에 알맞게 코드 초신화

--- head/define.h
+++ head/define.h
@@ -1,12 +1,22 @@
 #ifndef DEFINE_H
 # define DEFINE_H
 
+#define TRUE 1
+#define FALSE 0
+
+
+// Server
+#define NONE_INFO_SOCKET -404
+#define SOME_INFO_SOCKET  404
 
 // SCN[Sniffer code number defines]
+#define ERROR                   -1
 // [ERROR] (0 ~ 255)
-#define SCN_ERROR_SOCKET    0x0000000F
-#define SCN_ERROR_BIND      0x000000F0
-#define SCN_ERROR_ACCEPT    0x00000F00            
+#define SCN_ERROR_INFO_SOCKET   0   // 소켓에 대한 정보가 이상 
+#define SCN_ERROR_SOCKET        1   // 소켓 생성시 이상
+#define SCN_ERROR_BIND          2   // 소켓 바인드시 이상
+#define SCN_ERROR_LISTEN        3   // 소켓 listen시 이상
+#define SCN_ERROR_ACCEPT        4   // 소켓 Accept시 이상
 
 // [BUG] (256 ~ 512)
 
--- head/untils.h
+++ head/untils.h
@@ -1,12 +1,24 @@
 #ifndef UNTILS_H
 # define UNTILS_H
 
+// #include <stdio.h>
+#include <stdlib.h> // atoi
+#include <string.h> // memset
+#include <unistd.h> // sockaddr_in, read, write
+#include <arpa/inet.h> // htonl, htons, INADDR_ANY, sockaddr_in
+#include <sys/socket.h>
+// <UMS-By-C/srcs/server>
+
+    // tcp_server.c
+    int start_tcp_server(char *port[], struct sockaddr_in *serv_addr, struct sockaddr_in *clnt_addr, int listen_number);
+
 
 // [sniffer_code]
 
 // <sniffer_code.c>
 void sniffied(int SCN);
 
-// <>
+// <error.c>
+void print_error(int SCN);
 
 #endif
